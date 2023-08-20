c0d6117bb6a116624adf54c671c432fb950071ec
alvhix <alvhix@gmail.com>
2023-08-20 17:43:02 +0200

    Random order in repository tests to guarantee determinism

--- src/test/java/account/infrastructure/MariaDBAccountRepositoryTest.java
+++ src/test/java/account/infrastructure/MariaDBAccountRepositoryTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Execution(ExecutionMode.SAME_THREAD)
+@TestMethodOrder(MethodOrderer.Random.class)
 public class MariaDBAccountRepositoryTest {
     private AccountRepository accountRepository;
 
